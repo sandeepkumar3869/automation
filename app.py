97553c78260666b051a8a7867ff1eef91510aece
sandeep kumar <138839111+sandeepkumar3869@users.noreply.github.com>
2025-03-18 00:54:56 +0530

    Generated requirements.txt for deployment

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from oauth2client.service_account import ServiceAccountCredentials
 # Google Sheets authentication
 def authenticate_google_sheets():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    # creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
     # creds = ServiceAccountCredentials.from_json_keyfile_name("/etc/secrets/credentials.json", scope)
-    creds = ServiceAccountCredentials.from_json_keyfile_name("/opt/render/secrets/credentials.json", scope)
+    # creds = ServiceAccountCredentials.from_json_keyfile_name("/opt/render/secrets/credentials.json", scope)
     client = gspread.authorize(creds)
     return client
 
